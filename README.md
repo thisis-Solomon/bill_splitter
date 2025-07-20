605de7c90f5bba9135e0ee465e66062c6b738a25
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-07-20 16:09:54 +0200

    My attempt to the challenge

---
+++ README.md
@@ -0,0 +1,26 @@
+# Challenge: Simple Bill Splitter
+
+Write a Python script that helps split a bill evenly between friends.
+
+The programm should:
+
+1. Ask how many people are in the group.
+2. Ask for each person's name.
+3. Ask for the total bill amount.
+4. Calculate each person's share of the bill.
+5. Display how much each person owes in a clean readable format.
+
+Example:
+Total bill: 1200
+People: Aman, Neha, Ravi
+
+Each person owes: 400
+
+final output: 
+    Aman owes: 400
+    Neha owes: 400
+    Ravi owes: 400
+    
+Bonus:
+- Round to 2 decimal places
+- Print a decorative summury box
---
+++ bill_splitter.py
@@ -0,0 +1,22 @@
+
+number_in_the_group = input("How many people are in the group? ")
+names_of_the_group_members = input("Name of the member in the group separated with commas (Solomon, Njobvu) ").strip()
+total_bill = input("Enter the total bill: ").strip()
+
+def bill_spitter(number, names, total_bill):
+    
+    names = names.split(",")
+    number = int(number)
+    split = int(total_bill) / number
+    
+    if len(names) == number:
+        print(f'\nTotal bill: {total_bill}\nPeople: {names_of_the_group_members}\n\nEach person owes: {split}\n')
+        for name in names:
+             print(f"{name.strip()} owes: {round(split, 2)}")
+    else:
+        print(f'Name provided doesn\'t with the number of people in the group!' )
+    
+border = "*-*" * 30
+print(border)
+bill_spitter(number_in_the_group, names_of_the_group_members, total_bill)
+print(f"\n{border}")
